14b97486a832a88b2399e0e9f1e6c243f3045df4
leomols97 <leomols@hotmail.com>
2022-12-23 06:18:07 +0100

    Script should now be done

--- code/realtor/models/buyer_model.py
+++ code/realtor/models/buyer_model.py
@@ -3,7 +3,6 @@ from odoo import fields, models
 class Buyer (models.Model):
     _inherit = 'res.partner'
 
-    # _name = 'realtor.buyers'
     # apartment_ids = fields.Many2many('apartment', string='Appartements associés à l\'acheteur')
     # potential_buyers = fields.Many2many('realtor.offer', string='Offre liée à l\'acheteur')
     apartment = fields.Many2one('realtor.apartment', string="Offres de l'acheteur")
